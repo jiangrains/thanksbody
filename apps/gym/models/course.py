9ff302878d85174a58fd60edd860f8f71edf263b
jiangrains <jiangrains@126.com>
2015-11-17 17:56:00 +0800

    Update course.py

--- apps/gym/models/course.py
+++ apps/gym/models/course.py
@@ -1,4 +1,4 @@
-﻿#-*- encoding: utf-8 -*-
+#-*- encoding: utf-8 -*-
 from django.db import models
 from django.contrib import admin
 from shop.models.productmodel import Product
@@ -10,19 +10,34 @@ class BaseCourse(Product):
     COURSE_TYPE_MIDDLE_GROUP = 20
     COURSE_TYPE_SMALL_GROUP = 30
     COURSE_TYPE_PRIVATE_TEACH = 40
-    COURSE_TYPE_FREE_FITNESS = 50
-    
+    COURSE_TYPE_FREE_FITNESS = 50    
     COURSE_TYPE = (
         (COURSE_TYPE_LARGE_GROUP, u"大团体课"),
         (COURSE_TYPE_SMALL_GROUP, u"小团体课"),
         (COURSE_TYPE_PRIVATE_TEACH, u"私教课"),
-    ) 
+    )
+
+
+    COURSE_STATUS_SIGNUP = 10
+    COURSE_STATUS_RUNNING = 20
+    COURSE_STATUS_ENDED = 30
+    COURSE_STATUS_CANCEL = 40
+    COURSE_STATUS = (
+        (COURSE_STATUS_SIGNUP, u"报名中"),
+        (COURSE_STATUS_RUNNING, u"开课中"),
+        (COURSE_STATUS_ENDED, u"已结束"),
+        (COURSE_STATUS_CANCEL, u"已取消"),
+    )    
+    
     #name = models.CharField(u"课程名称", max_length=256)
     type = models.IntegerField(choices=COURSE_TYPE, default=COURSE_TYPE_LARGE_GROUP, verbose_name=u"课程类型")
-    gymnasium = models.OneToOneField(Gymnasium, null=True, blank=True, verbose_name=u"场馆")
-    coach = models.OneToOneField(BaseCoach, null=True, blank=True, verbose_name=u"教练")
-    min_person = models.IntegerField(verbose_name=u"最小开课人数", default=1)
-    max_person = models.IntegerField(verbose_name=u"最大开课人数", default=20)
+    gymnasium = models.ForeignKey(Gymnasium, null=True, blank=True, verbose_name=u"场馆")
+    coach = models.ForeignKey(BaseCoach, null=True, blank=True, verbose_name=u"教练")
+    class_cnt = models.IntegerField(default=0, verbose_name=u"课程节数")
+    min_user = models.IntegerField(default=1, verbose_name=u"最小开课人数")
+    max_user = models.IntegerField(default=20, verbose_name=u"最大开课人数")
+    user_cnt = models.IntegerField(default=0, verbose_name=u"实际报名人数")
+    status = models.IntegerField(choces=COURSE_STATUS, default=COURSE_STATUS_SIGNUP, verbose_name=u"课程状态")
 
     class Meta(object):
         abstract = False
@@ -33,8 +48,23 @@ class BaseCourse(Product):
 
     def __init__(self, *args, **kwargs):
         super(BaseCourse, self).__init__(*args, **kwargs)
+        self.status = COURSE_STATUS_SIGNUP
+        self.user_cnt = 0
         
     def __unicode__(self):
         return self.name
+
+class FitnessClass(models.Model):
+    course = models.ForeignKey(BaseCourse, related_name=u"classes", verbose_name=u"课程")
+    class_time = models.DateTimeField(auto_now=True, verbose_name=u"上课时间")
+    gymnasium = models.ForeignKey(Gymnasium, verbose_name=u"上课场馆")
+    room = models.IntegerField(default=0, verbose_name=u"上课房间")
+    check_in_cnt = models.IntegerField(verbose_name=u"实际上课人数")
+    
+class CheckInItem(models.Model):
+    user = models.ForeignKey(User, related_name=u"checkin_items", verbose_name=u"用户")
+    fitnessclass = models.ForeignKey(FitnessClass, related_name=u"checkin_items", verbose_name=u"课程")
+    date_added = models.DateTimeField(auto_now_add=True, verbose_name=u"签到时间")
+    
     
 admin.site.register(BaseCourse)    
