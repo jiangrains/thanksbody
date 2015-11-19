f7a5331f5ca92b96a6f48038ef356acc593a246f
jiangrains <jiangrains@126.com>
2015-11-19 09:15:26 +0800

    Update course.py

--- apps/gym/models/course.py
+++ apps/gym/models/course.py
@@ -6,15 +6,15 @@ from gymnasium import Gymnasium
 from coach import BaseCoach
     
 class BaseCourse(Product):     
-    COURSE_TYPE_LARGE_GROUP = 10
-    COURSE_TYPE_MIDDLE_GROUP = 20
-    COURSE_TYPE_SMALL_GROUP = 30
-    COURSE_TYPE_PRIVATE_TEACH = 40
-    COURSE_TYPE_FREE_FITNESS = 50    
+    COURSE_TYPE_GROUP = 10
+    COURSE_TYPE_PRIVATE_TEACH = 20    
+    COURSE_TYPE_COACH_ORDER = 30
+    COURSE_TYPE_FREE_FITNESS = 40
     COURSE_TYPE = (
-        (COURSE_TYPE_LARGE_GROUP, u"大团体课"),
-        (COURSE_TYPE_SMALL_GROUP, u"小团体课"),
+        (COURSE_TYPE_GROUP, u"团体课"),
         (COURSE_TYPE_PRIVATE_TEACH, u"私教课"),
+        (COURSE_TYPE_COACH_ORDER, u"教练预约课程"),
+        (COURSE_TYPE_FREE_FITNESS, u"自由健身"),        
     )
 
 
@@ -54,6 +54,13 @@ class BaseCourse(Product):
     def __unicode__(self):
         return self.name
 
+
+class UserCourse(models.Model):
+    user = models.ForeignKey(User, verbose_name=u"用户")
+    course = models.ForeignKey(User, verbose_name=u"课程")
+        
+        
+#所有的课表(class)        
 class FitnessClass(models.Model):
     course = models.ForeignKey(BaseCourse, related_name=u"classes", verbose_name=u"课程")
     class_time = models.DateTimeField(auto_now=True, verbose_name=u"上课时间")
